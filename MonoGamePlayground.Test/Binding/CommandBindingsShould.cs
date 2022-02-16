7538e06bcf67d76d881fe613ce96b7bfc86a5bab
Matt DeKrey <matt.dekrey@principlestudios.com>
2022-02-15 21:21:00 -0600

    Add tests

--- MonoGamePlayground.Test/Binding/CommandBindingsShould.cs
+++ MonoGamePlayground.Test/Binding/CommandBindingsShould.cs
@@ -21,4 +21,21 @@ public class CommandBindingsShould
         var compiled = (Func<float>)target.CreateValueLambda().Compile();
         Assert.Equal(1f, compiled());
     }
+
+    [Fact]
+    public void MutateTwoBindings()
+    {
+        // Arrange
+        var floatBinding1 = new Mock<Binding<float>>();
+        floatBinding1.Setup(mock => mock.CreateValueLambda()).Returns((LambdaExpression)(() => 2f));
+        var floatBinding2 = new Mock<Binding<float>>();
+        floatBinding2.Setup(mock => mock.CreateValueLambda()).Returns((LambdaExpression)(() => 3.5f));
+
+        // Act
+        var target = new Mutate<float, float, float>(floatBinding1.Object, floatBinding2.Object, (a, b) => a + b);
+
+        // Assert
+        var compiled = (Func<float>)target.CreateValueLambda().Compile();
+        Assert.Equal(5.5f, compiled());
+    }
 }
